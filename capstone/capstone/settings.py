81a145df30a0089750d6b4b2ec6be9bbe6ca6c35
vincentrondero <vincentpatrickrondero@gmail.com>
2023-10-28 23:06:49 +0800

    pipenv

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'capstone/static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'capstone/staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
