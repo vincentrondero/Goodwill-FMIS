c8f3cc70be71080c776a9c1cd85b7b2fac519ce0
vincentrondero <vincentpatrickrondero@gmail.com>
2023-10-28 22:59:29 +0800

    whitenoise

--- capstone/capstone/settings.py
+++ capstone/capstone/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'capstone/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
