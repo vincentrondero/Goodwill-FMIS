1fd5ffdb1ac86f0c0151d50b5691f6d9cd402e41
vincentrondero <vincentpatrickrondero@gmail.com>
2023-10-29 09:13:07 +0800

    deplo_try

--- capstone/settings.py
+++ capstone/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'capstone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Pigs',
         'USER': 'postgres',
         'PASSWORD': 'milotic03',
@@ -123,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'capstone/staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'capstone/static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 django_heroku.settings(locals())
